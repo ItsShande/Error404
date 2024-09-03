c79c3e926401cdadfcb1bc57d52925159a6b44ba
Ingrid <sensioncaballero@escuelasproa.edu.ar>
2024-09-03 14:14:29 -0300

    funcionandoInicioSesion

--- src/InicioSesion.jsx
+++ src/InicioSesion.jsx
@@ -1,25 +1,40 @@
-
+import { useState } from "react"
 function InicioSesion(){
+    const [Nombre,setNombre] = useState ('')
+    const [Contraseña,setContraseña] = useState ('')
+    const [Ubicacion,setUbicacion] = useState ('')
 
+    const agregarUsuario =()=>{
+        console.log(`${Nombre} ${Contraseña}${Ubicacion}`);
+    }
+    const updateNombre =(evento) =>{
+        setNombre(evento.target.value);
+    }
+    const updateContraseña =(evento) =>{
+        setContraseña(evento.target.value);
+    }
+    const updateUbicacion =(evento) =>{
+        setUbicacion(evento.target.value);
+    }
     return(
         <>
              <div>
       <form action="nombre">
         <i className="fa-solid fa-user"></i>
         <label id="nombre" htmlFor="name">Nombre</label>
-        <input type="text" name="Nombre" />
+        <input onChange={updateNombre} type="text" name="Nombre" />
       </form>
-      <form action="contrasena">
+      <form action="contraseña">
         <i className="fa-solid fa-key"></i>
         <label id="contra" htmlFor="contra">Contraseña</label>
-        <input type="password" name="Contraseña" />
+        <input onChange={updateContraseña} type="password" name="Contraseña" />
       </form>
       <form action="ciudad">
         <i className="fa-solid fa-location-dot"></i>
         <label id="ubi" htmlFor="ubi">Ubicación</label>
-        <input type="text" name="Ubicacion" />
+        <input onChange={updateUbicacion} type="text" name="Ubicacion" />
       </form>
-      <button>Siguiente</button>
+      <button onClick={agregarUsuario}>Siguiente</button>
     </div>
   </>
     )
