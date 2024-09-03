2dc01bf2f48a1074eaaae23ca6a50a39f4fc723a
Ingrid <sensioncaballero@escuelasproa.edu.ar>
2024-09-03 12:12:41 -0300

    Nav-Barr

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './Navbar'
 
 function App() {
     return (
         <>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"></link>
+        <Navbar/>
         </>
     )
 }
